36fc3e0c21e166d94bbd189625cc2801d06e88f8
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-04-26 13:50:51 -0400

    Added function to normalize tokens

--- Parser.java
+++ Parser.java
@@ -1,5 +1,46 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.PrintWriter; // Import the PrintWriter class to write text files
+
 public class Parser {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        Scanner inStream = null; // initialize read
+        PrintWriter outStream=null; // initialize write
+
+        //try/catch for accessing input file to account for errors
+        try
+        {
+            inStream = new Scanner(new File("prog4TMTokens.txt"));
+        } // end try
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Error opening the file prog4TMTokens.txt.");
+            System.exit(0);
+        }
+
+        //try/catch for accessing output file to account for errors
+        try
+        {
+            outStream = new PrintWriter("Prog4TMout.txt");
+        } // end try
+        catch(FileNotFoundException e)
+        {
+            System.out.println("error opening the file Prog4TMout.txt.");
+            System.exit(0);
+        } // end catch
+    }
+
+    public static boolean START() {
+        System.out.println("Entering function: START");
+        
+        return true; 
+    }
+
+    public static String toLower(String input) {
+        //Per instructions: "The language is not case sensitive." All input is converted to lower case to conform to logic of parser.
+
+        return input.toLowerCase();
     }
 }
