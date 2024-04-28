91bea2cc9383d3702f8f3a49e536f858676f7928
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-04-28 14:28:13 -0400

    Refactored and uncommented Strict Grammar Version

--- Parser.java
+++ Parser.java
@@ -1,9 +1,17 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList; // Import the ArrayList class
+import java.util.List; // Import the List class
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.io.PrintWriter; // Import the PrintWriter class to write text files
+
+// START -> { COMMANDS }
+// COMMANDS -> COMMAND ‘[‘ (REGISTER | VALUE )  [ ‘,’ REGISTER] ‘]’
+// COMMAND -> ‘st’ | ‘ld’ | ‘sub’ | ‘add’ | ‘sq’  | ‘rt’ 
+// REGISTER -> ‘a’ | ‘b’ | ‘c’ | ‘d’ | ‘e’ | ‘f’
+// VALUE -> DIGITS
+// DIGIT -> ‘0’ | .. | ‘9’
+// DIGITS -> DIGIT {DIGIT}
 
 public class Parser {
     public static void main(String[] args) {
@@ -33,48 +41,30 @@ public class Parser {
             System.exit(0);
         } // end catch
         
-        //call the Start grammar rule
-
         START(inStream, outStream);
 
-        inStream.close();
-        outStream.close();
+        inStream.close(); // close the input file
+        outStream.close(); // close the output file
     }
 
-    // START -> { COMMANDS }
-    // COMMANDS -> COMMAND ‘[‘ (REGISTER | VALUE )  [ ‘,’ REGISTER] ‘]’
-    // COMMAND -> ‘st’ | ‘ld’ | ‘sub’ | ‘add’ | ‘sq’  | ‘rt’ 
-    // REGISTER -> ‘a’ | ‘b’ | ‘c’ | ‘d’ | ‘e’ | ‘f’
-    // VALUE -> DIGITS
-    // DIGIT -> ‘0’ | .. | ‘9’
-    // DIGITS -> DIGIT {DIGIT}
-
-
     public static boolean START(Scanner inStream,PrintWriter outStream) {
-
-        //loop will be here
-
         System.out.println("Entering function: START");
         outStream.println("Entering function: START");
 
         while (inStream.hasNextLine()) {
-            // Read the next line
 
-            if (COMMANDS(inStream, outStream)) {
-                //return true;
-            }
-            else {
+            if (!COMMANDS(inStream, outStream)) {
                 System.out.println("Leaving function: START Failure");
                 outStream.println("Leaving function: START Failure");
                 System.exit(0);
                 return false;
             }
+            
         }
 
         System.out.println("Leaving function: START Success");
         outStream.println("Leaving function: START Success");
         return true;
-
     }
 
     public static boolean COMMANDS(Scanner inStream, PrintWriter outStream) {
@@ -86,71 +76,55 @@ public class Parser {
         if (COMMAND(inStream, outStream, command)) {
             try {
                 String nextLine = getNextLine(inStream);
-                if (nextLine != null && nextLine.equals("[")) {
-                    System.out.println("Bracket Good!");
-                } 
-                else {
-                    System.out.println("bracket bad");
+                if (!nextLine.equals("[")) {
                     System.out.println("Leaving function: COMMANDS Failure");
                     outStream.println("Leaving function: COMMANDS Failure");
                     return false;
-                }
+                } 
             } catch (NullPointerException e) {
-                System.out.println("bracket bad");
                 System.out.println("Leaving function: COMMANDS Failure");
                 outStream.println("Leaving function: COMMANDS Failure");
                 return false;
             }
-
                 
             String registerValueLine = toLower(getNextLine(inStream));
 
             if (REGISTER(outStream, registerValueLine) == 1) {
-                System.out.println("Register Good!"); 
-
+                
                 //optional part of the grammar
-
                 try {
                     String nextLine = getNextLine(inStream);
-                    if (nextLine != null && nextLine.equals(",")) {
-                        System.out.println("optional comma Good!");
-                    } 
-                    else if(nextLine != null && nextLine.equals("]")){
-                        System.out.println("bracket detected leaving early!");
+                    if(nextLine.equals("]")){
                         System.out.println("Leaving function: COMMANDS Success");
                         outStream.println("Leaving function: COMMANDS Success");
                         return true;
                     }
-                    else {
-                        System.out.println("no comma or bracket... nothing there");
+                    else if( !nextLine.equals(",")){
                         System.out.println("Leaving function: COMMANDS Failure");
                         outStream.println("Leaving function: COMMANDS Failure");
                         return false;
                     }
                 } catch (NullPointerException e) {
-                    System.out.println("no comma or bracket... nothing there");
                     System.out.println("Leaving function: COMMANDS Failure");
                     outStream.println("Leaving function: COMMANDS Failure");
                     return false;
                 }
 
                 if( REGISTER(outStream, toLower(getNextLine(inStream))) == 1) {
-                    System.out.println("Register Good!");
 
                     try {
                         String nextLine = getNextLine(inStream);
                         if (nextLine != null && nextLine.equals("]")) {
-                            System.out.println("required bracket Good!");
+                            System.out.println("Leaving function: COMMANDS Success");
+                            outStream.println("Leaving function: COMMANDS Success");
                             return true;
                         } 
                         else {
-                            System.out.println("no bracket... nothing there");
                             System.out.println("Leaving function: COMMANDS Failure");
                             outStream.println("Leaving function: COMMANDS Failure");
                             return false;
                         }
                     } catch (NullPointerException e) {
-                        System.out.println("no comma or bracket... nothing there");
                         System.out.println("Leaving function: COMMANDS Failure");
                         outStream.println("Leaving function: COMMANDS Failure");
                         return false;
@@ -161,54 +135,42 @@ public class Parser {
                     outStream.println("Leaving function: COMMANDS Failure");
                     return false;
                 }
-
             }
             else if (VALUE(outStream, registerValueLine) == 2) {
-                System.out.println("Value Good!");
-
                 //optional part of the grammar
-                
                 try {
                     String nextLine = getNextLine(inStream);
-                    if (nextLine != null && nextLine.equals(",")) {
-                        System.out.println("optional comma Good!");
-                    } 
-                    else if(nextLine != null && nextLine.equals("]")){
-                        System.out.println("bracket detected leaving early!");
+                    if(nextLine.equals("]")){
                         System.out.println("Leaving function: COMMANDS Success");
                         outStream.println("Leaving function: COMMANDS Success");
                         return true;
                     }
-                    else {
-                        System.out.println("no comma or bracket... nothing there");
+                    else if( !nextLine.equals(",")){
                         System.out.println("Leaving function: COMMANDS Failure");
                         outStream.println("Leaving function: COMMANDS Failure");
                         return false;
                     }
                 } catch (NullPointerException e) {
-                    System.out.println("no comma or bracket... nothing there");
                     System.out.println("Leaving function: COMMANDS Failure");
                     outStream.println("Leaving function: COMMANDS Failure");
                     return false;
                 }
 
                 if( REGISTER(outStream, toLower(getNextLine(inStream))) == 1) {
-                    System.out.println("Register Good!");
 
                     try {
                         String nextLine = getNextLine(inStream);
                         if (nextLine != null && nextLine.equals("]")) {
-                            System.out.println("required bracket Good!");
+                            System.out.println("Leaving function: COMMANDS Success");
+                            outStream.println("Leaving function: COMMANDS Success");
                             return true;
                         } 
                         else {
-                            System.out.println("no bracket... nothing there");
                             System.out.println("Leaving function: COMMANDS Failure");
                             outStream.println("Leaving function: COMMANDS Failure");
                             return false;
                         }
                     } catch (NullPointerException e) {
-                        System.out.println("no comma or bracket... nothing there");
                         System.out.println("Leaving function: COMMANDS Failure");
                         outStream.println("Leaving function: COMMANDS Failure");
                         return false;
@@ -219,36 +181,22 @@ public class Parser {
                     outStream.println("Leaving function: COMMANDS Failure");
                     return false;
                 }
-
-                
-
             }
             else {
                 System.out.println("Leaving function: COMMANDS Failure");
                 outStream.println("Leaving function: COMMANDS Failure");
                 return false;
             }
-            
-            
-
         }
 
         System.out.println("Leaving function: COMMANDS Failure");
         outStream.println("Leaving function: COMMANDS Failure");
         return false;
-        
     }
 
     public static boolean COMMAND(Scanner inStream, PrintWriter outStream, String command) {
         System.out.println("Entering function: COMMAND");
         outStream.println("Entering function: COMMAND");
-
-        if (command == null) {
-            System.out.println("Leaving function: COMMAND Failure");
-            outStream.println("Leaving function: COMMAND Failure");
-            System.exit(0);
-            return false;
-        }
 
         if (command.equals("st") || command.equals("ld") || command.equals("sub") || command.equals("add") || command.equals("sq") || command.equals("rt")) {
             System.out.println("Leaving function: COMMAND Success");
@@ -257,7 +205,6 @@ public class Parser {
         } else {
             System.out.println("Leaving function: COMMAND Failure");
             outStream.println("Leaving function: COMMAND Failure");
-            System.exit(0);
             return false;
         }
     }
@@ -266,11 +213,7 @@ public class Parser {
         System.out.println("Entering function: REGISTER");
         outStream.println("Entering function: REGISTER");
 
-        System.out.println("Register Value Line: " + registerValueLine);
-
         if (registerValueLine.equals("a") || registerValueLine.equals("b") || registerValueLine.equals("c") || registerValueLine.equals("d") || registerValueLine.equals("e") || registerValueLine.equals("f")) {
-            //System.out.println("Command Good!");
-            //outStream.println("Command Good!");
             System.out.println("Leaving function: REGISTER Success");
             outStream.println("Leaving function: REGISTER Success");
             return 1;
@@ -319,23 +262,21 @@ public class Parser {
                     return false;
                 }
             }
-            
         }
-
         else {
             System.out.println("Leaving function: DIGITS Failure");
             outStream.println("Leaving function: DIGITS Failure");
             return false;
         }
 
+        System.out.println("Leaving function: DIGITS Success");
+        outStream.println("Leaving function: DIGITS Success");
         return true;
     }
 
     public static boolean DIGIT(String registerValueLine,PrintWriter outStream, int digitCounter) {
         System.out.println("Entering function: DIGIT");
         outStream.println("Entering function: DIGIT");
-
-        System.out.println("Register Value Line: " + registerValueLine);
 
         if (digitCounter == 0) {
             if (registerValueLine.equals("0") || registerValueLine.equals("1") || registerValueLine.equals("2") || registerValueLine.equals("3") || registerValueLine.equals("4") || registerValueLine.equals("5") || registerValueLine.equals("6") || registerValueLine.equals("7") || registerValueLine.equals("8") || registerValueLine.equals("9")){
@@ -365,7 +306,7 @@ public class Parser {
     }
 
     public static String toLower(String input) {
-        //Per instructions: "The language is not case sensitive." All input is converted to lower case to conform to logic of parser.
+        //"The language is not case sensitive."
         return input.toLowerCase();
     }
 
